dd4f461556f54cc8d5aa34ccb627e5f953001ccd
Fabio Neto <fabiogvdneto@gmail.com>
2023-11-18 11:49:08 +0000

    Successfully implemented login command. Added some validations and finished sending message using UDP.

--- user.c
+++ user.c
@@ -7,58 +7,140 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-
-#define DEFAULT_PORT "58069"
+#include <ctype.h>
 
 #define PORT_FLAG "-p"
 #define IP_FLAG "-n"
 
-char hostname[128];
-char ip[INET_ADDRSTRLEN];
-char port[6];
+#define DEFAULT_PORT 58069
+#define DEFAULT_IP "127.0.0.1"
 
-void set_default_ip() {
-    if (gethostname(hostname, sizeof(hostname)) == -1) {
-        fprintf(stderr, "error: %s\n", strerror(errno));
-        exit(-1);
+struct sockaddr_in server_addr;
+
+/* ---- UDP Protocol ---- */
+
+void udp_send_message(char *msg) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1) {
+        exit(1);
     }
 
-    struct addrinfo hints, *res;
-    struct in_addr *addr;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-
-    int errcode = getaddrinfo(hostname, NULL, &hints, &res);
-
-    if (errcode) {
-        fprintf(stderr, "error: getaddrinfo: %s\n", gai_strerror(errcode));
-        exit(-1);
+    ssize_t bytes = sendto(fd, msg, sizeof(msg), 0, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    if (bytes == -1) {
+        exit(1);
     }
 
-    while (res) {
-        addr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
-        inet_ntop(res->ai_family, addr, ip, sizeof(ip));
-        res = res->ai_next;
+    printf("Messsage sent (%ld bytes).\n", bytes);
+    close(fd);
+}
+
+/* ---- Validators ---- */
+
+int validate_user_uid(char *str) {
+    if (strlen(str) != 6) return 0;
+    
+    while (*str != '\0') {
+        if (!isdigit(*str++)) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int validate_password(char *str) {
+    if (strlen(str) != 8) return 0;
+
+    while (*str != '\0') {
+        if (!isalnum(*str++)) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+/* ---- Commands ---- */
+
+void login(char *command) {
+    char uid[7], pwd[9];
+    sscanf(command, "login %s %s\n", uid, pwd);
+    uid[6] = pwd[8] = '\0';
+
+    if (!validate_user_uid(uid)) {
+        printf("The UID must be a 6-digit IST student number.\n");
+        return;
+    }
+
+    if (!validate_password(pwd)) {
+        printf("The password must be composed of 8 alphanumeric characters.\n");
+        return;
+    }
+
+    char msg[20];
+    sprintf(msg, "LIN %s %s", uid, pwd);
+    udp_send_message(msg);
+}
+
+/* ---- Command Listener ---- */
+
+void parse_command() {
+    char buffer[100];
+
+    while (1) {
+        printf("> ");
+        fgets(buffer, sizeof(buffer), stdin);
+
+        int n = strchr(buffer, ' ') - buffer;
+        
+        if (!strncmp(buffer, "login", n)) {
+            login(buffer);
+        } else if (!strncmp(buffer, "logout", n)) {
+
+        } else if (!strncmp(buffer, "unregister", n)) {
+            
+        } else if (!strncmp(buffer, "exit", n)) {
+            
+        } else if (!strncmp(buffer, "open", n)) {
+            
+        } else if (!strncmp(buffer, "close", n)) {
+            
+        } else if (!strncmp(buffer, "myactions", n) || !strncmp(buffer, "ma", n)) {
+            
+        } else if (!strncmp(buffer, "mybids", n) || !strncmp(buffer, "mb", n)) {
+            
+        } else if (!strncmp(buffer, "list", n) || !strncmp(buffer, "l", n)) {
+            
+        } else if (!strncmp(buffer, "show_asset", n) || !strncmp(buffer, "sa", n)) {
+            
+        } else if (!strncmp(buffer, "bid", n)) {
+            
+        } else if (!strncmp(buffer, "show_record", n)) {
+            
+        } else {
+            printf("Command not found.\n");
+        }
     }
 }
 
+/* ---- Initialization ---- */
+
 int main(int argc, char **argv) {
-    set_default_ip();
-    strcpy(port, DEFAULT_PORT);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(DEFAULT_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], IP_FLAG)) {
-            strcpy(ip, argv[++i]);
-        } else if (strcmp(argv[i], PORT_FLAG)) {
-            strcpy(port, argv[++i]);
+        if (!strcmp(argv[i], IP_FLAG)) {
+            server_addr.sin_addr.s_addr = inet_addr(argv[++i]);
+        } else if (!strcmp(argv[i], PORT_FLAG)) {
+            server_addr.sin_port = htons(atoi(argv[++i]));
         } else {
             printf("tu es estupido vai po crl");
             exit(0);
         }
     }
 
-    printf("%s %s\n", ip, port);
+    parse_command();
     return 1;
 }
